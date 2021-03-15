0e15cb1839dd591539a6d079585030a6458e71d5
jiwonseo <jiwonseo@LAPTOP-9TH84017>
2021-03-15 16:10:37 +0900

    project start

---
+++ trade/src/test/java/com/trade/project/TradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
